a248f980337b23b41c4dd237895e56a6df2d3ac8
Bates <florian@cegledi.net>
2024-05-24 17:49:47 +0200

    feat: :sparkles: Add check for newer versions

--- scripts/storage.py
+++ scripts/storage.py
@@ -1,6 +1,10 @@
 import sqlite3
 from os import getenv, mkdir, path
 from semver import Version
+from time import time
+from requests import get
+from urllib import request
+from nicegui import ui
 from _version import __version__
 
 
@@ -45,7 +49,44 @@ class StorageInterface():
         # TODO: If compare returns 1, an update was installed. Check for DB updates?
         if version == None or Version.parse(__version__[1:]).compare(version[1:]) == 1:
             self.set_config('version', __version__)
+        self._check_for_updates()
         self.save()
+
+    def _check_for_updates(self):
+        last_check = float(self.get_config('last_update_check'))
+        if last_check != None and time() < last_check + (60 * 5):  # Only allow an update check every 5 minutes.
+            print("Skipping check!")
+            return
+        
+        rate_limit = None
+        try:
+            rate_limit = get('https://api.github.com/rate_limit').json()
+        except:  # If we cannot fetch rate limit information, abort to be sure
+            return
+        
+        if rate_limit['resources']['core']['remaining'] < 5:  # If we have less than 5 requests remaining, abort
+            return
+
+        versions = None
+        try:
+            versions = get('https://api.github.com/repos/b4t3s/warthunder-translation-editor/tags').json()
+        except:
+            return  # Again, if we cannot request, we might be getting rate-limited. Abort any further action
+        
+        self.set_config('last_update_check', time())
+
+        if Version.parse(versions[0]['name'][1:]).compare(__version__[1:]) == 1:
+            with ui.dialog() as diag:
+                with ui.card():
+                    with ui.row():
+                        ui.label('New version available!')
+                        ui.badge(__version__, color='negative')
+                        ui.icon('east')
+                        ui.badge(versions[0]['name'], color='positive')
+                    with ui.row():
+                        ui.button('Download', on_click=lambda: request.urlopen('https://github.com/B4T3S/Warthunder-Translation-Editor/releases'), color='positive', icon='download')
+                        ui.button('Skip for now', on_click=diag.close, color='gray')
+                diag.open()
 
     def get_config(self, key: str, default = None):
         cur = self._database.cursor()
